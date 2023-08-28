7101640e0566f11f9d02191ca367942aec7214b0
tameem.j <44329967+tameem-jahangir@users.noreply.github.com>
2023-08-28 15:09:20 +0200

    finished project

---
+++ components/ProjectActions.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+
+import Link from 'next/link'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { deleteProject, fetchToken } from '@/lib/actions'
+import { useRouter } from 'next/navigation'
+
+export const ProjectActions = ({ projectId } : { projectId: string} ) => {
+
+    const router = useRouter()
+    const [isDeleting, setIsDeleting] = useState(false)
+
+    const handleDeleteProject = async () => {
+        setIsDeleting(true)
+
+        const{ token } = await fetchToken();
+
+        try {
+            await deleteProject(projectId, token)
+
+            router.push('/')
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsDeleting(false)
+        }
+    }
+  return (
+    <>
+        <Link href={`/edit-project/${projectId}`} 
+        className="flexCenter edit-action_btn">
+            <Image src="/pencile.svg" width={25} height={15} alt="edit"/>
+        </Link>
+
+        <button type="button" 
+            className={`flexCenter delete-action_btn ${isDeleting ? 'bg-gray' : 'bg-primary-purple'}`}
+            onClick={handleDeleteProject}>
+            <Image src="/trash.svg" width={25} height={15} alt="delete"/>
+        </button>
+    </>
+  )
+}
--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,21 +1,22 @@
 "use client"
 
-import { SessionInterface } from "@/common.types"
+import { ProjectInterface, SessionInterface } from "@/common.types"
 import { ChangeEvent, useState } from "react"
 import Image from "next/image"
 import FormField from "./FormField"
 import { categoryFilters } from "@/constants"
 import CustomMenu from "./CustomMenu"
 import Button from "./Button"
-import { createNewProject, fetchToken } from "@/lib/actions"
+import { createNewProject, fetchToken, updateProject } from "@/lib/actions"
 import { useRouter } from "next/navigation"
 
 type Props = {
     type: string,
     session: SessionInterface
+    project?: ProjectInterface
 }
 
-const ProjectForm = ( {type, session}: Props) => {
+const ProjectForm = ( {type, session, project}: Props) => {
     const router = useRouter()
 
     const handleFormSubmit = async (e: React.FormEvent) => {
@@ -28,6 +29,12 @@ const ProjectForm = ( {type, session}: Props) => {
         try {
             if(type === 'create') {
                 await createNewProject(form, session?.user?.id, token)
+
+                router.push('/')
+            }
+
+            if(type === 'edit') {
+                await updateProject(form, project?.id as string, token)
 
                 router.push('/')
             }
@@ -66,12 +73,12 @@ const ProjectForm = ( {type, session}: Props) => {
 
     const [isSubmitting, setIsSubmitting] = useState(false)
     const [form, setform] = useState({
-        title:'',
-        description:'',
-        image:'',
-        liveSiteUrl:'',
-        githubUrl:'',
-        category:'',
+        title: project?.title || '',
+        description: project?.description || '',
+        image: project?.image || '',
+        liveSiteUrl: project?.liveSiteUrl || '',
+        githubUrl: project?.githubUrl || '',
+        category: project?.category || '',
     })
 
 
