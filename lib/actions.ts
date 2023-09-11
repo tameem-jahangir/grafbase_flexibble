c20c3a4cf187b4d6abff613e04787510c9b0672e
tameem.j <44329967+tameem-jahangir@users.noreply.github.com>
2023-09-11 13:57:19 +0200

    bug fix with grafbase

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,7 @@ import { GraphQLClient } from "graphql-request";
 
 import { createProjectMutation, createUserMutation, deleteProjectMutation, updateProjectMutation, getProjectByIdQuery, getProjectsOfUserQuery, getUserQuery, projectsQuery } from "@/graphql";
 import { ProjectForm } from "@/common.types";
+import { categoryFilters } from "@/constant";
 
 const isProduction = process.env.NODE_ENV === 'production';
 const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
@@ -43,8 +44,10 @@ const makeGraphQLRequest = async (query: string, variables = {}) => {
 
 export const fetchAllProjects = (category?: string | null, endcursor?: string | null) => {
   client.setHeader("x-api-key", apiKey);
+  
+  const categories = category == null ? categoryFilters : [category];
 
-  return makeGraphQLRequest(projectsQuery, { category, endcursor });
+  return makeGraphQLRequest(projectsQuery, { categories, endcursor });
 };
 
 export const createNewProject = async (form: ProjectForm, creatorId: string, token: string) => {
